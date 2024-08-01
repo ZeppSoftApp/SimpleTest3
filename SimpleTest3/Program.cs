17f624e1e7d25d41421593051791bd57fdd39757
Zepp <zeppsoftapp@gmail.com>
2024-08-01 10:49:30 +0300

    INIT

--- SimpleTest3/Program.cs
+++ SimpleTest3/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter your name:");
+        string name = string.Empty;
+        name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
     }
 }
 
